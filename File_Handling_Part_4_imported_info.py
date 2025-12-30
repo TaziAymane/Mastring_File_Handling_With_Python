a2559f47d4b18855ca5f563661e2c741fff269c5
Tazi Aymane <taziaymane68@gmail.com>
2025-12-30 23:07:12 +0100

    Files Handling Part 4 Important Info

---
+++ File_Handling_Part_4_imported_info.py
@@ -0,0 +1,15 @@
+# -------------------------------------
+# -- File Handling => Important Info --
+# -------------------------------------
+
+# myFile = open(r'C:\Users\hp pro\Desktop\EST_D\S5\python\newFile.txt'  , "a")
+# myFile.truncate(5)
+# print(myFile.tell())
+
+# myFile = open(r'C:\Users\hp pro\Desktop\EST_D\S5\python\newFile.txt'  , "r")
+# myFile.seek(5)
+# print(myFile.read())
+# myFile.close()
+
+import os
+os.remove(r'C:\Users\hp pro\Desktop\EST_D\S5\python\fun.txt')
