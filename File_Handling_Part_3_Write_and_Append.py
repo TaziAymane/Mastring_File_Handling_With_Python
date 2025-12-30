e747c9cb834ef3f0ff87e75ed5c5fc3723211eda
Tazi Aymane <taziaymane68@gmail.com>
2025-12-30 22:54:21 +0100

    Files Handling Part 3 Write and Append In Files

---
+++ File_Handling_Part_3_Write_and_Append.py
@@ -0,0 +1,20 @@
+# -------------------------------------
+# -- File Handling => Write & Appeds --
+# ------------------------------------- 
+
+# myFile = open(r'C:\Users\hp pro\Desktop\EST_D\S5\python\newFile.txt' , 'w')
+# myFile.write('Hello from auther File \n')
+# myFile.write('second Line ')
+
+# myFile = open(r'C:\Users\hp pro\Desktop\EST_D\S5\python\fun.txt' , 'w')
+# myFile.write('Hello\n' * 100)
+
+# myList = ["ahmed\n" , "samir\n" , "karim\n"]
+# myFile = open(r'C:\Users\hp pro\Desktop\EST_D\S5\python\newFile.txt' , 'w')
+# myFile.writelines(myList)
+
+
+myFile = open(r'C:\Users\hp pro\Desktop\EST_D\S5\python\newFile.txt' , "a")
+# myFile.write('Hello\n')
+# myFile.write('second line is here ')
+myFile.write('here is new line\n')
